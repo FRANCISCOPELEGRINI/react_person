4058db28fbed62801411b21978923d99ea1d3bc7
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-09-07 16:53:43 -0300

    dsadada

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-
 export const Person = ({ person }) => {
+  const partnerInfo = person.isMarried
+    ? person.sex === "m"
+      ? <p className="Person__partner">{person.partnerName} is my wife</p>
+      : <p className="Person__partner">{person.partnerName} is my husband</p>
+    : <p className="Person__partner">I am not married</p>;
+
+  return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
       {"age" in person
-        ? <p className="Person__age">{person.age}</p>
-        : <p className="Person__age">Age unknown</p>
+        ? <p className="Person__age">I am {person.age}</p>
+        : null
       }
 
-      {person.isMarried
-        ? person.sex === "m"
-          ? <p className="Person__partner">{person.partnerName} is my wife</p>
-          : <p className="Person__partner">{person.partnerName} is my husband</p>
-        : <p className="Person__partner">I am not married</p>
-      }
+      {partnerInfo}
     </section>
+  );
 };
